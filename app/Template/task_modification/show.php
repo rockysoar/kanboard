2793e46bfb9b621c2ac1c4d25bdf4f85f5a5506d
junhua.zhang <junhua.zhang@fanli.com>
2019-03-20 16:52:32 +0800

    分类字段作为task的项目标识

--- app/Template/task_modification/show.php
+++ app/Template/task_modification/show.php
@@ -9,6 +9,7 @@
             <?= $this->task->renderTitleField($values, $errors) ?>
             <?= $this->task->renderDescriptionField($values, $errors) ?>
             <?= $this->task->renderDescriptionTemplateDropdown($project['id']) ?>
+            <?= $this->task->renderCategoryField($categories_list, $values, $errors) ?>
             <?= $this->task->renderTagField($project, $tags) ?>
 
             <?= $this->hook->render('template:task:form:first-column', array('values' => $values, 'errors' => $errors)) ?>
@@ -17,7 +18,6 @@
         <div class="task-form-secondary-column">
             <?= $this->task->renderColorField($values) ?>
             <?= $this->task->renderAssigneeField($users_list, $values, $errors) ?>
-            <?= $this->task->renderCategoryField($categories_list, $values, $errors) ?>
             <?= $this->task->renderPriorityField($project, $values) ?>
 
             <?= $this->hook->render('template:task:form:second-column', array('values' => $values, 'errors' => $errors)) ?>
@@ -28,8 +28,6 @@
             <?= $this->task->renderStartDateField($values, $errors) ?>
             <?= $this->task->renderTimeEstimatedField($values, $errors) ?>
             <?= $this->task->renderTimeSpentField($values, $errors) ?>
-            <?= $this->task->renderScoreField($values, $errors) ?>
-            <?= $this->task->renderReferenceField($values, $errors) ?>
 
             <?= $this->hook->render('template:task:form:third-column', array('values' => $values, 'errors' => $errors)) ?>
         </div>
